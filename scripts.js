79202f43d27ac178def7835c63be9105a85b8510
ashleyjoseph021 <ashleyjoseph01@proton.me>
2025-04-19 01:39:13 -0600

    fixed clock to be 12 hr instead of 24 hr

--- scripts.js
+++ scripts.js
@@ -4,10 +4,25 @@ function startTime() {
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds();
+    // let s = today.getSeconds();
     m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+    // s = checkTime(s);
+
+    let ampm;
+    console.log(h);
+    if (h == 0) {
+        ampm = "AM";
+      h = 12;
+    } else if (h == 12) {
+        ampm = "PM";
+    } else if (h > 12) {
+        ampm = "PM";
+      h = h - 12;
+    } else {
+        ampm = "AM";
+    }
+  
+    document.getElementById('time').innerHTML =  h + ":" + m + " " + ampm;
     setTimeout(startTime, 1000);
   }
   
